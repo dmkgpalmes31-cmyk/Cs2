d4bda7325b17622db27f83df349f33ddaa990654
dmkgpalmes31-cmyk <dmkgpalmes31@src.pshs.edu.ph>
2026-08-11 09:48:02 +0800

    Update Q1_Acacia_FA1_Palmes.py

--- Q1_Acacia_FA1_Palmes.py
+++ Q1_Acacia_FA1_Palmes.py
@@ -1,9 +1,9 @@
 import math # to make functions exclusive to the math library work
 
-X1 = float(input("Enter coordinate X:"))
-Y1 = float(input("Enter coordinate Y:"))
-X2 = float(input("Enter coordinate X:"))
-Y2 = float(input("Enter coordinate Y:"))
+X1 = float(input("Enter coordinate X:")) # coordinate 1
+Y1 = float(input("Enter coordinate Y:")) # coordinate 1
+X2 = float(input("Enter coordinate X:")) # coordinate 2
+Y2 = float(input("Enter coordinate Y:")) # coordinate 2
 
 distance = math.sqrt(math.pow(X2 - X1, 2) + math.pow(Y2 - Y1, 2))
                      
