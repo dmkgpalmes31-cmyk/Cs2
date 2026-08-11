164ac67bb89f800a45b6f90d7d9bd7b1a44d1bf1
dmkgpalmes31-cmyk <dmkgpalmes31@src.pshs.edu.ph>
2026-08-11 09:51:06 +0800

    Update Q1_Acacia_FA1_Palmes.py

--- Q1_Acacia_FA1_Palmes.py
+++ Q1_Acacia_FA1_Palmes.py
@@ -5,6 +5,6 @@ Y1 = float(input("Enter coordinate Y:")) # coordinate 1
 X2 = float(input("Enter coordinate X:")) # coordinate 2
 Y2 = float(input("Enter coordinate Y:")) # coordinate 2
 
-distance = math.sqrt(math.pow(X2 - X1, 2) + math.pow(Y2 - Y1, 2))
+distance = math.sqrt(math.pow(X2 - X1, 2) + math.pow(Y2 - Y1, 2)) # Equation of the 
                      
 print(f"The Distance between the coordinate is, {distance:.2f}")
